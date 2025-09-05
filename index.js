ddb0bbed8d2605ed1a789e73357e2a1a47a573ae
Mirek Ratman <mirek.ratman@gmail.com>
2025-09-05 23:21:34 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+function loadChili() {
+    const token = document.getElementById('token').value;
+    const environmentBaseURL = document.getElementById('url').value;
+    const projectID = document.getElementById('projectID').value;
+    const projectName = 'test';
+    const userInterfaceID = document.getElementById('interfaceID').value;
+    const refreshTokenAction = () => Promise.resolve(token);
+
+    window.StudioUI.studioUILoaderConfig({
+    selector: 'studio-ui-container',
+    projectDownloadUrl: `${environmentBaseURL}/projects/${projectID}/document`,
+    projectUploadUrl: `${environmentBaseURL}/projects/${projectID}`,
+    projectId: projectID,
+    graFxStudioEnvironmentApiBaseUrl: environmentBaseURL,
+    authToken: token,
+    refreshTokenAction: refreshTokenAction,
+    projectName: projectName,
+    userInterfaceID: userInterfaceID,
+    });
+
+}
+
